dceffc70bfd4034402eb6d40fffca35f4444f124
massoumou yann <YannPC>
2025-09-28 17:28:05 +0200

    Created and observable

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,31 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Y');
+title = 'Angular-Observable';
+
+data: any[] = [];
+
+//Observable
+myObsersable = new Observable((observer) => {
+  // this data will be emitted after 1 second
+  setTimeout(() => observer.next('Angular'), 1000);
+  setTimeout(() => observer.next('React'), 2000);
+  setTimeout(() => observer.next('Vue'), 3000);
+  setTimeout(() => observer.next('NodeJs'), 4000);
+  setTimeout(() => observer.complete(), 5000);
+
+  
+})
+   GetAsynData(){
+
+    //Observer 
+    //next, error, complete
+    this.myObsersable.subscribe((value: any) => {
+      // this.myObsersable.subcrible is the observer and ((value:any)=>{}) is the Handler function
+    // value just provided by observable that the observer is subscribed to
+    // any because we have data have any as data type
+      this.data.push(value);
+    }); 
+  };
 }
+
