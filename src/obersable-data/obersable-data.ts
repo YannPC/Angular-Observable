34e266eb1b86c0544a88ae0c1ce1e67fa843592c
YannMassoumou <YannPC>
2026-07-07 23:38:48 +0200

    refactor

--- src/obersable-data/obersable-data.ts
+++ src/obersable-data/obersable-data.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { DataProviderService } from '../services/data-provider-service';
 
 @Component({
   selector: 'app-obersable-data',
   imports: [],
   templateUrl: './obersable-data.html',
-  styleUrl: './obersable-data.css'
+  styleUrl: './obersable-data.css',
 })
 export class ObersableData {
+  private dataProviderService = inject(DataProviderService);
 
+  protected readonly comments = signal<string>(
+    'Angular is a platform for building mobile and desktop web applications.',
+  );
 }
